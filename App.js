3300bc2bf648cd724ec8670c63ccb6ef58ce6b43
henni saaranen <n0sahe00@students.oamk.fi>
2022-09-27 19:47:08 +0300

    add to repo

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Platform, Pressable, StyleSheet, Text, View } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function App() {
+
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+
+  const onChange = (_, selectDate) => {
+    if (Platform.OS === 'ios') {
+      setShow(false);
+    }
+
+    const currentDate = selectDate || date;
+    setDate(currentDate);
+  }
+
+  const toggle = () => {
+    setShow(prevShow => !prevShow)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {show && Platform.OS === 'ios' && (
+        <DateTimePicker
+          style={{width: 320}}
+          mode={'date'}
+          display="inline"
+          value={date}
+          onChange={onChange}
+          />
+      )}
+      {show && Platform.OS === 'android' && (
+        <DateTimePicker
+          mode={'date'}
+          display="default"
+          value={date}
+          onChange={onChange}
+          />
+      )}
+      <Pressable
+        onPress={toggle}>
+          <Text>{date.getDate()}.{date.getMonth()+1}.{date.getFullYear()}</Text>
+      </Pressable>
+
     </View>
   );
 }
